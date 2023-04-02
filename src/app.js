4aa2d73dba6f2dfb8be561de3cd47c060669c2d1
tripn11 <noblenwala.nn@gmail.com>
2023-04-02 21:50:11 +0100

    latest versions

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import "core-js/stable"; //to replace babel/polyfill
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'normalize.css/normalize.css';
-import './style.scss';
+import 'react-dates/lib/css/_datepicker.css';
+import './styles/style.scss';
 
 
-const App = ()=>(
-    <p>i love you</p>
+const App = ()=> (    
+  <div>
+    <p>hello world</p>
+  </div>  
 )
 
 const root = createRoot(document.getElementById('app')); 
-root.render(<App />);
+root.render(<App />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
-const path = require('path'); 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 require('dotenv').config();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //alllows for the html template
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //creates a seperate css file
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'; //sets the environment whether in production or not, else development
 
-module.exports = (env,argv) => { 
-  const isProduction = argv.mode === 'production'; //argv.mode reads the mode whether in production or development
+module.exports = (env,argv) => {
+  const isProduction = argv.mode === 'production'; //argv.mode reads the mode whether in production or development, wouldn't have been necessary if NODE_ENV worked
 
   return {
     entry: path.resolve(__dirname,'src/app.js'),
 
     output: {
       filename: 'bundle.js',
-      path: path.resolve(__dirname,'dist'), 
-      clean: true
+      path: path.resolve(__dirname,'dist'), //put the output in a folder known as dist in the root folder
+      clean: true //deletes the folder and creates a new one anytime the project is built
     },
 
     module: {
@@ -25,54 +22,50 @@ module.exports = (env,argv) => {
         {
         loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
-        }, 
+        exclude: /node_modules/ //sll files ending in .js except those in node_modules should be translated by babel
+        },
         {
         test: /\.s?css$/,
         use: [
           isProduction ?//use style loader in dev mode and minicssextract in production mode
-            MiniCssExtractPlugin.loader:"style-loader",
+            MiniCssExtractPlugin.loader:"style-loader", //adds the style tag to the string to make it a style. the only difference between the 2 is that minicssextract creates a seperate file for the style
             {
-              loader: "css-loader",
+              loader: "css-loader", //gathers all css into a string
               options: {
-                sourceMap: true 
+                sourceMap: true
               }
             },
             {
-              loader: "sass-loader",
+              loader: "sass-loader", //gathers all sass into a string
               options: {
-                sourceMap: true 
+                sourceMap: true
               }
             }
         ]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i, //for reading images of different formats
+          type: 'asset/resource',
         }
-
       ]
     },
-    
-    devtool: isProduction ? 'source-map' : 'inline-source-map', 
+
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
 
     plugins: [
       new HtmlWebpackPlugin({
-        title:"Boiler Plate",
+        title:"GlobalTrain",
         filename:"index.html",
-        template:"src/template.html"
+        template:"src/template.html",
+        // favicon:"src/Images/logo.png" //don't add the ./
       }),
-      new MiniCssExtractPlugin(),
-      new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-      })
+      new MiniCssExtractPlugin()
     ],
     devServer: {
       static: {
         directory:path.resolve(__dirname,'dist')
       },
-      historyApiFallback: true
+      historyApiFallback: true //load the index.html file instead of an error when a client refreshes because the route is not in the server
     },
   };
 };
